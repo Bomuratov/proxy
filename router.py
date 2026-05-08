7cc18f248ccda0dd1ade430eff86cd41e0ed8d36
Bomuratov <antikko99@gmail.com>
2026-05-08 12:33:34 +0500

    wq

--- router.py
+++ router.py
@@ -91,17 +91,17 @@ def send_verification(payload: VerifyCode):
         data = response.json()
         text = f"OTP успешно отправлен на номер +{payload.phone},\nOTP код: {payload.otp}\nОтвет от ESKIZ:\n<pre language='json'>{data}</pre>"
 
-#        bot.send_message(
-#          text=text,
-#        )
+        bot.send_message(
+            text=text,
+        )
 
         return data
 
     except requests.RequestException as e:
         error_text = f"Ошибка при отправке OTP на номер +{payload.phone}\nOTP: {payload.otp}\nОшибка: {str(e)}"
-#         bot.send_message(
-#             text=error_text,
-#         )
+        bot.send_message(
+            text=error_text,
+        )
 
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
